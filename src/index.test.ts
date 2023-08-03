1162fe2b8f823a9008814c118dd9f8875efadf91
Sam Huynh <sam.huynh@lyka.com.au>
2023-08-03 16:02:05 +1000

    chore: stop test from spamming the console

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,6 +2,7 @@ import { describe, it, expect, vi } from 'vitest';
 import { sayHello } from './index';
 
 const consoleSpy = vi.spyOn(console, 'log');
+consoleSpy.mockImplementation(() => { });
 
 describe('say hello', () => {
   it('should say hello', () => {
